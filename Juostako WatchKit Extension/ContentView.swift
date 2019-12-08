586941de3ae19be3345d424f75594288b8939ea8
Martin Richter <me@martinrichter.net>
2019-12-08 12:27:39 +0100

    Initial Commit

---
+++ Juostako WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Juostako WatchKit Extension
+//
+//  Created by Martin Richter on 08.12.19.
+//  Copyright © 2019 Martin Richter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Juostako WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Juostako WatchKit Extension
+//
+//  Created by Martin Richter on 08.12.19.
+//  Copyright © 2019 Martin Richter. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
